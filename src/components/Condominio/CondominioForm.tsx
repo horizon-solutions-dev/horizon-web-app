1efa83050eab2329dffb0990df4db50537e1f7f5
michelmartins9164 <michelmartins9164@gmail.com>
2026-05-31 19:16:52 -0300

    ajuste

--- src/components/Condominio/CondominioForm.tsx
+++ src/components/Condominio/CondominioForm.tsx
@@ -479,10 +479,7 @@ const CondominioForm: React.FC<CondominioFormProps> = ({
       nextErrors.condominiumType = t("condominioForm.typeRequired");
     }
     if (!formData.physicalStructureId) {
-      nextErrors.physicalStructureId =
-        t("condominioForm.physicalStructureRequired", {
-          defaultValue: "Selecione a estrutura fisica.",
-        });
+      nextErrors.physicalStructureId = "Estrutura física é obrigatória.";
     }
     return nextErrors;
   };
